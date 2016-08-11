ed2c3766d6343271e989a739abb902d60c087788
blomdahldaniel <blomdahl.daniel@gmail.com>
2016-08-11 13:38:33 +0200

    spam protect mailto-links with JavaScript

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -219,9 +219,56 @@
   <div class="footer">
     <a href="https://github.com/wictorstenseke/bokningssystem-HTK" target="_blank"><i class="fa fa-github"></i></a>
     |
-    <a href="mailto:wictor.johansson@gmail.com" target="_blank">Wictor Johansson</a>
+    <script type="text/javascript" language="javascript">
+    <!--
+    // Email obfuscator script 2.1 by Tim Williams, University of Arizona
+    // Random encryption key feature by Andrew Moulden, Site Engineering Ltd
+    // This code is freeware provided these four comment lines remain intact
+    // A wizard to generate this code is at http://www.jottings.com/obfuscator/
+    { coded = "T3dHA7.NAPs4ccA4@8ps3o.dAp"
+      key = "l9un0wvOcYjS8Vp167aqhMHDkioBe4xTJWd2NRXyQKZ5sbPLgIm3ArCEGUtfFz"
+      shift=coded.length
+      link=""
+      for (i=0; i<coded.length; i++) {
+        if (key.indexOf(coded.charAt(i))==-1) {
+          ltr = coded.charAt(i)
+          link += (ltr)
+        }
+        else {
+          ltr = (key.indexOf(coded.charAt(i))-shift+key.length) % key.length
+          link += (key.charAt(ltr))
+        }
+      }
+    document.write("<a href='mailto:"+link+"'>Wictor Johansson</a>")
+    }
+    //-->
+    </script><noscript>Sorry, you need Javascript on to email me.</noscript>
     |
-    <a href="mailto:blomdahl.daniel@gmail.com" target="_blank">Daniel Blomdahl</a>
+    <script type="text/javascript" language="javascript">
+    <!--
+    // Email obfuscator script 2.1 by Tim Williams, University of Arizona
+    // Random encryption key feature by Andrew Moulden, Site Engineering Ltd
+    // This code is freeware provided these four comment lines remain intact
+    // A wizard to generate this code is at http://www.jottings.com/obfuscator/
+    { coded = "XAgB61tA.61ldWA@SB1dA.ZgB"
+      key = "BS3RQ7YieshzrobxUyVAM4DLq5HP8aCndWctpmgXwT9Iu0K2JjNvOf1Fl6GZEk"
+      shift=coded.length
+      link=""
+      for (i=0; i<coded.length; i++) {
+        if (key.indexOf(coded.charAt(i))==-1) {
+          ltr = coded.charAt(i)
+          link += (ltr)
+        }
+        else {
+          ltr = (key.indexOf(coded.charAt(i))-shift+key.length) % key.length
+          link += (key.charAt(ltr))
+        }
+      }
+    document.write("<a href='mailto:"+link+"'>Daniel Blomdahl</a>")
+    }
+    //-->
+    </script><noscript>Sorry, you need Javascript on to email me.</noscript>
+
   </div>
 
 
