890786e28819a5c2d008670a8bdcdabeb0efea8d
Wictor Stenseke <wictor.stenseke@knowit.se>
2018-06-08 21:53:35 +0200

    added microcopy at submit_form_button

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -93,6 +93,7 @@
         value="{{ old('name') }}"
       >
       <br>
+      <p style="margin-top: 12px; margin-bottom: -8px; opacity: 0.5;font-style: italic; font-size: 13px;">Har du kollat så tiden är ledig?</p>
       <input type="submit" value="Bekräfta bokning" class="submit-button">
     </form>
   </div>
