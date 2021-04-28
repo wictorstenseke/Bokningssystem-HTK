d3f914b54f7acdd947de89f261d66f82dc7f8001
Wictor Stenseke <wictor.stenseke@knowit.se>
2021-04-28 15:58:03 +0200

    change class of arsmote

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -38,11 +38,11 @@
   <h3>Välkommen till <br> Högelids Tennisklubb!</h3>
   <p class="intro-text">Här kan du som är medlem boka speltid på vår grusbana och se andra medlemmars bokade tider.</p>
 
-  <div class="bokade-tider">
-    <h2>Årsmöte 28 april kl. 20.00</h2>
+  <div class="kontakt-sektion">
+    <h2>Årsmöte 28 april 20.00</h2>
     <div class="kontaktinfo">
       <p style="margin-top: 4px;">Klicka på knappen för att delta i årsmötet online. Det skall fungera på både dator och telefon.</p>
-      <p>Om något krånglar går det bra att ringa Wictor på 0768 99 54 35</p>
+      <p>Om något krånglar går det bra att ringa Wictor på tel. 076 899 54 35.</p>
       <a href="https://meet.jit.si/hogelidstennis2021" target="_blank" class="boka-pike">Gå till årsmöte</a>
     </div>
   </div>
